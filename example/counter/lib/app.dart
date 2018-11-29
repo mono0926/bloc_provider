3651d3755230c28dca8d1dd7e764cebad48f64cc
Masayuki Ono <mono0926@gmail.com>
2018-11-29 21:45:45 +0900

    Move counter example

--- example/counter/lib/app.dart
+++ example/counter/lib/app.dart
@@ -1,7 +1,8 @@
-import 'package:bloc_provider_example/bloc/counter_bloc_provider.dart';
-import 'package:bloc_provider_example/const.dart';
-import 'package:bloc_provider_example/page/counter_page.dart';
 import 'package:flutter/material.dart';
+
+import 'bloc/counter_bloc_provider.dart';
+import 'const.dart';
+import 'page/counter_page.dart';
 
 class App extends StatelessWidget {
   @override
