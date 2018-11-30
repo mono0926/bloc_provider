5d88427c8a95d88e2efa91baeb22e1a70d0cbf59
Masayuki Ono <mono0926@gmail.com>
2018-11-30 20:39:39 +0900

    Add bloc to builder argument

--- lib/src/bloc_provider.dart
+++ lib/src/bloc_provider.dart
@@ -3,16 +3,18 @@ import 'package:flutter/widgets.dart';
 
 typedef _BlocCreator<BlocType extends Bloc> = BlocType Function(
     BuildContext context);
+typedef BlocBuilder<BlocType extends Bloc> = Widget Function(
+    BuildContext context, BlocType bloc);
 
 class BlocProvider<BlocType extends Bloc> extends StatefulWidget {
-  final WidgetBuilder childBuilder;
+  final BlocBuilder<BlocType> childBuilder;
   final _BlocCreator<BlocType> creator;
 
   BlocProvider({
     @required Widget child,
     @required _BlocCreator<BlocType> creator,
   }) : this.builder(
-          childBuilder: (context) => child,
+          childBuilder: (_context, _bloc) => child,
           creator: creator,
         );
 
@@ -42,7 +44,7 @@ class _BlocProviderState<BlocType extends Bloc>
   Widget build(BuildContext context) {
     return _Inherited<BlocType>(
       bloc: _bloc,
-      child: Builder(builder: (context) => widget.childBuilder(context)),
+      child: widget.childBuilder(context, _bloc),
     );
   }
 
